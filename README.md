14d44c7b5e0f7bcd79a4e79db5f705534af8a142
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2020-07-19 14:32:17 +0200

    Project Setup (Closes #1) (PR #2)
    * Started with setup
    
    * Working on setup
    
    * Readme and better gitignore
    
    * Readme and easier setup
    
    * allow fixmes for now

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# DagInspector
+# MLInspect
+Inspect ML Pipelines in Python in the form of a DAG
+
+## Run MLInspect locally
+
+Prerequisite: python >=  3.7
+
+1. Clone this repository
+2. Set up the environment
+
+	`cd MLInspect` <br>
+	`python -m venv venv` <br>
+	`source venv/bin/activate` <br>
+	
+3. Install dependencies 
+
+    `python setup.py develop` <br>
+
+3. Run the tests
+
+    `python setup.py test` <br>
---
+++ setup.py
@@ -0,0 +1,37 @@
+"""
+For 'python setup.py develop' and 'python setup.py test'
+"""
+import os
+from setuptools import setup, find_packages
+
+ROOT = os.path.dirname(__file__)
+
+with open(os.path.join(ROOT, "requirements", "requirements.txt")) as f:
+    required = f.read().splitlines()
+
+with open(os.path.join(ROOT, "requirements", "requirements.dev.txt")) as f:
+    test_required = f.read().splitlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="MLInspect",
+    version="0.0.1",
+    description="Inspect ML Pipelines in the form of a DAG",
+    author='Stefan Grafberger',
+    author_email='stefangrafberger@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    include_package_data=True,
+    setup_requires=test_required,
+    tests_require=test_required,
+    install_requires=required + test_required,  # FIXME: remove this later, just for develop convenience
+    license='Apache License 2.0',
+    python_requires='>=3',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3 :: Only'
+    ]
+)
