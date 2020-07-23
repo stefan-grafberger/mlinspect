f936d715183bb203d69302e72d92ac0a1985ae2d
stefan-grafberger <stefangrafberger@gmail.com>
2020-07-23 11:40:39 +0200

    Extracting wir for the test adult_eady pipeline works

--- test/instrumentation/test_wir_extractor.py
+++ test/instrumentation/test_wir_extractor.py
@@ -2,9 +2,12 @@
 Tests whether the WIR extraction works
 """
 import ast
+import os
 from inspect import cleandoc
+from mlinspect.utils import get_project_root
 from mlinspect.instrumentation.wir_extractor import WirExtractor, Vertex
 
+FILE_PY = os.path.join(str(get_project_root()), "test", "pipelines", "adult_easy.py")
 
 def test_print_stmt():
     """
@@ -12,12 +15,12 @@ def test_print_stmt():
     """
     extractor = WirExtractor()
     test_ast = ast.parse("print('test')")
-    extracted_dag = extractor.extract_wir(test_ast)
+    extracted_wir = extractor.extract_wir(test_ast)
 
     expected_constant = Vertex(0, "test", [], "Constant")
     expected_call = Vertex(1, "print", [expected_constant], "Call")
     expected_graph = [expected_constant, expected_call]
-    assert extracted_dag == expected_graph
+    assert extracted_wir == expected_graph
 
 
 def test_print_var_usage():
@@ -29,12 +32,12 @@ def test_print_var_usage():
         print(test_var)""")
     extractor = WirExtractor()
     test_ast = ast.parse(test_code)
-    extracted_dag = extractor.extract_wir(test_ast)
+    extracted_wir = extractor.extract_wir(test_ast)
     expected_constant = Vertex(0, "test", [], "Constant")
     expected_assign = Vertex(1, "test_var", [expected_constant], "Assign")
     expected_call = Vertex(2, "print", [expected_assign], "Call")
     expected_graph = [expected_constant, expected_assign, expected_call]
-    assert extracted_dag == expected_graph
+    assert extracted_wir == expected_graph
 
 
 def test_string_call_attribute():
@@ -46,12 +49,12 @@ def test_string_call_attribute():
         """)
     extractor = WirExtractor()
     test_ast = ast.parse(test_code)
-    extracted_dag = extractor.extract_wir(test_ast)
+    extracted_wir = extractor.extract_wir(test_ast)
     expected_constant_one = Vertex(0, "hello ", [], "Constant")
     expected_constant_two = Vertex(1, "world", [], "Constant")
     expected_attribute_call = Vertex(2, "join", [expected_constant_one, expected_constant_two], "Call")
     expected_graph = [expected_constant_one, expected_constant_two, expected_attribute_call]
-    assert extracted_dag == expected_graph
+    assert extracted_wir == expected_graph
 
 
 def test_print_expressions():
@@ -63,12 +66,12 @@ def test_print_expressions():
         """)
     extractor = WirExtractor()
     test_ast = ast.parse(test_code)
-    extracted_dag = extractor.extract_wir(test_ast)
+    extracted_wir = extractor.extract_wir(test_ast)
     expected_constant = Vertex(0, "test", [], "Constant")
     expected_call_one = Vertex(1, "isupper", [expected_constant], "Call")
     expected_call_two = Vertex(2, "print", [expected_call_one], "Call")
     expected_graph = [expected_constant, expected_call_one, expected_call_two]
-    assert extracted_dag == expected_graph
+    assert extracted_wir == expected_graph
 
 
 def test_keyword():
@@ -80,7 +83,7 @@ def test_keyword():
         """)
     extractor = WirExtractor()
     test_ast = ast.parse(test_code)
-    extracted_dag = extractor.extract_wir(test_ast)
+    extracted_wir = extractor.extract_wir(test_ast)
     expected_constant_one = Vertex(0, "comma", [], "Constant")
     expected_constant_two = Vertex(1, "separated", [], "Constant")
     expected_constant_three = Vertex(2, "words", [], "Constant")
@@ -90,7 +93,7 @@ def test_keyword():
                                         expected_keyword], "Call")
     expected_graph = [expected_constant_one, expected_constant_two, expected_constant_three, expected_constant_four,
                       expected_keyword, expected_call]
-    assert extracted_dag == expected_graph
+    assert extracted_wir == expected_graph
 
 
 def test_import():
@@ -104,12 +107,12 @@ def test_import():
         """)
     extractor = WirExtractor()
     test_ast = ast.parse(test_code)
-    extracted_dag = extractor.extract_wir(test_ast)
+    extracted_wir = extractor.extract_wir(test_ast)
     expected_import = Vertex(0, "math", [], "Import")
     expected_constant = Vertex(1, "4", [], "Constant")
     expected_constant_call = Vertex(2, "sqrt", [expected_import, expected_constant], "Call")
     expected_graph = [expected_import, expected_constant, expected_constant_call]
-    assert extracted_dag == expected_graph
+    assert extracted_wir == expected_graph
 
 
 def test_import_as():
@@ -123,12 +126,12 @@ def test_import_as():
             """)
     extractor = WirExtractor()
     test_ast = ast.parse(test_code)
-    extracted_dag = extractor.extract_wir(test_ast)
+    extracted_wir = extractor.extract_wir(test_ast)
     expected_import = Vertex(0, "math", [], "Import")
     expected_constant = Vertex(1, "4", [], "Constant")
     expected_constant_call = Vertex(2, "sqrt", [expected_import, expected_constant], "Call")
     expected_graph = [expected_import, expected_constant, expected_constant_call]
-    assert extracted_dag == expected_graph
+    assert extracted_wir == expected_graph
 
 
 def test_import_from():
@@ -142,12 +145,12 @@ def test_import_from():
             """)
     extractor = WirExtractor()
     test_ast = ast.parse(test_code)
-    extracted_dag = extractor.extract_wir(test_ast)
+    extracted_wir = extractor.extract_wir(test_ast)
     expected_import = Vertex(0, "math", [], "Import")
     expected_constant = Vertex(1, "4", [], "Constant")
     expected_constant_call = Vertex(2, "sqrt", [expected_import, expected_constant], "Call")
     expected_graph = [expected_import, expected_constant, expected_constant_call]
-    assert extracted_dag == expected_graph
+    assert extracted_wir == expected_graph
 
 
 def test_nested_import_from():
@@ -161,12 +164,12 @@ def test_nested_import_from():
             """)
     extractor = WirExtractor()
     test_ast = ast.parse(test_code)
-    extracted_dag = extractor.extract_wir(test_ast)
+    extracted_wir = extractor.extract_wir(test_ast)
     expected_import = Vertex(0, "mlinspect.utils", [], "Import")
     expected_call_one = Vertex(1, "get_project_root", [expected_import], "Call")
     expected_call_two = Vertex(2, "print", [expected_call_one], "Call")
     expected_graph = [expected_import, expected_call_one, expected_call_two]
-    assert extracted_dag == expected_graph
+    assert extracted_wir == expected_graph
 
 
 def test_list_creation():
@@ -178,13 +181,13 @@ def test_list_creation():
             """)
     extractor = WirExtractor()
     test_ast = ast.parse(test_code)
-    extracted_dag = extractor.extract_wir(test_ast)
+    extracted_wir = extractor.extract_wir(test_ast)
     expected_constant_one = Vertex(0, "test1", [], "Constant")
     expected_constant_two = Vertex(1, "test2", [], "Constant")
     expected_list = Vertex(2, "as_list", [expected_constant_one, expected_constant_two], "List")
     expected_call = Vertex(3, "print", [expected_list], "Call")
     expected_graph = [expected_constant_one, expected_constant_two, expected_list, expected_call]
-    assert extracted_dag == expected_graph
+    assert extracted_wir == expected_graph
 
 
 def test_index_subscript():
@@ -199,7 +202,7 @@ def test_index_subscript():
             """)
     extractor = WirExtractor()
     test_ast = ast.parse(test_code)
-    extracted_dag = extractor.extract_wir(test_ast)
+    extracted_wir = extractor.extract_wir(test_ast)
     expected_import = Vertex(0, "pandas", [], "Import")
     expected_constant_one = Vertex(1, "test_path", [], "Constant")
     expected_call = Vertex(2, "read_csv", [expected_import, expected_constant_one], "Call")
@@ -208,7 +211,7 @@ def test_index_subscript():
     expected_index_subscript = Vertex(5, "Index-Subscript", [expected_assign, expected_constant_two], "Subscript")
     expected_graph = [expected_import, expected_constant_one, expected_call, expected_assign,
                       expected_constant_two, expected_index_subscript]
-    assert extracted_dag == expected_graph
+    assert extracted_wir == expected_graph
 
 
 def test_tuples():
@@ -222,7 +225,7 @@ def test_tuples():
             """)
     extractor = WirExtractor()
     test_ast = ast.parse(test_code)
-    extracted_dag = extractor.extract_wir(test_ast)
+    extracted_wir = extractor.extract_wir(test_ast)
     expected_import_from = Vertex(0, "sklearn", [], "Import")
     expected_constant_one = Vertex(1, "categorical", [], "Constant")
     expected_constant_two = Vertex(2, "ignore", [], "Constant")
@@ -234,9 +237,23 @@ def test_tuples():
     expected_tuple = Vertex(8, "as_tuple", [expected_constant_one, expected_call, expected_list], "Tuple")
     expected_graph = [expected_import_from, expected_constant_one, expected_constant_two, expected_keyword,
                       expected_call, expected_constant_three, expected_constant_four, expected_list, expected_tuple]
-    assert extracted_dag == expected_graph
+    assert extracted_wir == expected_graph
 
-    # TODO: actual pipeline code, nested calls, functions with multiple return values
+
+def test_adult_easy_pipeline():
+    """
+    Tests whether the WIR Extraction works for the adult_easy pipeline
+    """
+    with open(FILE_PY) as file:
+        test_code = file.read()
+
+        extractor = WirExtractor()
+        test_ast = ast.parse(test_code)
+        extracted_wir = extractor.extract_wir(test_ast)
+
+        assert len(extracted_wir) == 59
+
+    # TODO: Functions with multiple return values, function defs and other edge cases
     # Maybe mark caller explicitly?
 
     # We can also see if we really need runtime detection to check where a function comes from.
