5c7238ba78b5a4339591f5f72a27fb3b5ca538bb
stefan-grafberger <stefangrafberger@gmail.com>
2020-07-27 14:31:34 +0200

    Minor DagVertex __repr__ changes

--- mlinspect/instrumentation/dag_vertex.py
+++ mlinspect/instrumentation/dag_vertex.py
@@ -12,21 +12,21 @@ class DagVertex:
         # pylint: disable=too-many-arguments
         self.node_id = node_id
         self.name = name
+        self.module = module
         self.lineno = lineno
         self.col_offset = col_offset
-        self.module = module
 
     def __repr__(self):
-        message = "DagVertex(node_id={}: name='{}', lineno={}, col_offset={}, module={})" \
-            .format(self.node_id, self.name, self.lineno, self.col_offset, self.module)
+        message = "DagVertex(node_id={}: name='{}', module={}, lineno={}, col_offset={})" \
+            .format(self.node_id, self.name, self.module, self.lineno, self.col_offset)
         return message
 
     def __eq__(self, other):
         return self.node_id == other.node_id and \
                self.name == other.name and \
+               self.module == other.module and \
                self.lineno == other.lineno and \
-               self.col_offset == other.col_offset and \
-               self.module == other.module
+               self.col_offset == other.col_offset
 
     def __hash__(self):
         return hash(self.node_id)
--- test/instrumentation/test_dag_vertex.py
+++ test/instrumentation/test_dag_vertex.py
@@ -10,4 +10,4 @@ def test_dag_repr():
     """
     vertex = DagVertex(1, "Data Source", 1, 5, ('pandas.io.parsers', 'read_csv'))
     assert str(vertex) == "DagVertex(node_id=1: name='Data Source', " \
-                          "lineno=1, col_offset=5, module=('pandas.io.parsers', 'read_csv'))"
+                          "module=('pandas.io.parsers', 'read_csv'), lineno=1, col_offset=5)"
