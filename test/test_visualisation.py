213a71e468608e0e1118d6e31b96f1ca39c06d22
stefan-grafberger <stefangrafberger@gmail.com>
2020-07-29 22:22:57 +0200

    trying to fix travis build

--- test/test_visualisation.py
+++ test/test_visualisation.py
@@ -3,9 +3,9 @@ Tests whether the visualisation of the resulting DAG works
 """
 import os
 
-from mlinspect.pipeline_inspector import PipelineInspector
 from mlinspect.utils import get_project_root
 from mlinspect.visualisation import save_fig_to_path
+from test.utils import get_expected_dag_adult_easy_py
 
 FILE_PY = os.path.join(str(get_project_root()), "test", "pipelines", "adult_easy.py")
 
@@ -15,10 +15,7 @@ def test_inspector_py_pipeline():
     Tests whether the .py version of the inspector works
     """
     # pylint: disable=duplicate-code
-    extracted_dag = PipelineInspector\
-        .on_pipeline_from_py_file(FILE_PY)\
-        .add_analyzer("test")\
-        .execute()
+    extracted_dag = get_expected_dag_adult_easy_py()
 
     filename = os.path.join(str(get_project_root()), "test", "pipelines", "adult_easy.png")
     save_fig_to_path(extracted_dag, filename)
