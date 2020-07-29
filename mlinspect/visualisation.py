5c7002e5e2276f543accac001bfb93383588dba6
stefan-grafberger <stefangrafberger@gmail.com>
2020-07-29 23:22:52 +0200

    Add a very simple graph as string

--- mlinspect/visualisation.py
+++ mlinspect/visualisation.py
@@ -11,7 +11,6 @@ def save_fig_to_path(extracted_dag, filename):
     """
     Create a figure of the extracted DAG and save it with some filename
     """
-    # pylint: disable=duplicate-code
 
     def get_new_node_label(node):
         label = cleandoc("""
@@ -27,3 +26,19 @@ def save_fig_to_path(extracted_dag, filename):
     agraph = to_agraph(extracted_dag)
     agraph.layout('dot')
     agraph.draw(filename)
+
+
+def get_dag_as_pretty_string(extracted_dag):
+    """
+    Create a figure of the extracted DAG and save it with some filename
+    """
+
+    def get_new_node_label(node):
+        label = "{}: {}({}, {})".format(node.node_id, node.operator_name, node.module[0], node.module[1])
+        return label
+
+    # noinspection PyTypeChecker
+    extracted_dag = networkx.relabel_nodes(extracted_dag, get_new_node_label)
+
+    agraph = to_agraph(extracted_dag)
+    return agraph.to_string()
--- test/test_visualisation.py
+++ test/test_visualisation.py
@@ -5,12 +5,12 @@ import os
 
 from test.utils import get_expected_dag_adult_easy_py
 from mlinspect.utils import get_project_root
-from mlinspect.visualisation import save_fig_to_path
+from mlinspect.visualisation import save_fig_to_path, get_dag_as_pretty_string
 
 FILE_PY = os.path.join(str(get_project_root()), "test", "pipelines", "adult_easy.py")
 
 
-def test_inspector_py_pipeline():
+def test_save_fig_to_path():
     """
     Tests whether the .py version of the inspector works
     """
@@ -20,3 +20,14 @@ def test_inspector_py_pipeline():
     save_fig_to_path(extracted_dag, filename)
 
     assert os.path.isfile(filename)
+
+
+def test_get_dag_as_pretty_string():
+    """
+    Tests whether the .py version of the inspector works
+    """
+    extracted_dag = get_expected_dag_adult_easy_py()
+
+    pretty_string = get_dag_as_pretty_string(extracted_dag)
+
+    print(pretty_string)
