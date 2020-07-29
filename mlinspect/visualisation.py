2dcbf4d69546293734ba5b2bd5f742a6dc917f1d
stefan-grafberger <stefangrafberger@gmail.com>
2020-07-29 21:10:46 +0200

    Adding very simple visualisation

---
+++ mlinspect/visualisation.py
@@ -0,0 +1,29 @@
+"""
+Utility functions to visualise the extracted DAG
+"""
+from inspect import cleandoc
+
+import networkx
+from networkx.drawing.nx_agraph import to_agraph
+
+
+def save_fig_to_path(extracted_dag, filename):
+    """
+    Create a figure of the extracted DAG and save it with some filename
+    """
+    # pylint: disable=duplicate-code
+
+    def get_new_node_label(node):
+        label = cleandoc("""
+                {}: {}
+                {}
+                {}
+                """.format(node.node_id, node.operator_name, node.module[0], node.module[1]))
+        return label
+
+    # noinspection PyTypeChecker
+    extracted_dag = networkx.relabel_nodes(extracted_dag, get_new_node_label)
+
+    agraph = to_agraph(extracted_dag)
+    agraph.layout('dot')
+    agraph.draw(filename)
