bf6df61e16efce65263324c08e21097a9487c33f
stefan-grafberger <stefangrafberger@gmail.com>
2020-08-18 13:54:39 +0200

    All tests work again after modifying code_reference

--- test/instrumentation/analyzers/test_materialize_first_rows_analyzer.py
+++ test/instrumentation/analyzers/test_materialize_first_rows_analyzer.py
@@ -37,7 +37,7 @@ def get_expected_result():
     """
     expected_result = {
         DagNode(node_id=18, operator_type=OperatorType.DATA_SOURCE, module=('pandas.io.parsers', 'read_csv'),
-                code_reference=CodeReference(lineno=12, col_offset=11), description='adult_train.csv'): [
+                code_reference=CodeReference(12, 11, 12, 62), description='adult_train.csv'): [
                     AnalyzerInputRow(
                         values=[46, 'Private', 128645, 'Some-college', 10, 'Divorced', 'Prof-specialty',
                                 'Not-in-family', 'White', 'Female', 0, 0, 40, 'United-States', '<=50K'],
@@ -52,7 +52,7 @@ def get_expected_result():
                                 'sex', 'capital-gain', 'capital-loss', 'hours-per-week',
                                 'native-country', 'income-per-year'])],
         DagNode(node_id=20, operator_type=OperatorType.SELECTION, module=('pandas.core.frame', 'dropna'),
-                code_reference=CodeReference(lineno=14, col_offset=7), description='dropna'): [
+                code_reference=CodeReference(14, 7, 14, 24), description='dropna'): [
                     AnalyzerInputRow(
                         values=[46, 'Private', 128645, 'Some-college', 10, 'Divorced', 'Prof-specialty',
                                 'Not-in-family', 'White', 'Female', 0, 0, 40, 'United-States', '<=50K'],
@@ -68,17 +68,17 @@ def get_expected_result():
                                 'capital-gain', 'capital-loss', 'hours-per-week',
                                 'native-country', 'income-per-year'])],
         DagNode(node_id=23, operator_type=OperatorType.PROJECTION, module=('pandas.core.frame', '__getitem__'),
-                code_reference=CodeReference(lineno=16, col_offset=38), description="to ['income-per-year']"): [
+                code_reference=CodeReference(16, 38, 16, 61), description="to ['income-per-year']"): [
                     AnalyzerInputRow(values=['<=50K'], fields=['income-per-year']),
                     AnalyzerInputRow(values=['<=50K'], fields=['income-per-year'])],
         DagNode(node_id=28, operator_type=OperatorType.PROJECTION_MODIFY,
                 module=('sklearn.preprocessing._label', 'label_binarize'),
-                code_reference=CodeReference(lineno=16, col_offset=9),
+                code_reference=CodeReference(16, 9, 16, 89),
                 description="label_binarize, classes: ['>50K', '<=50K']"): [
                     AnalyzerInputRow(values=[array(1)], fields=['array']),
                     AnalyzerInputRow(values=[array(1)], fields=['array'])],
         DagNode(node_id=56, operator_type=OperatorType.TRAIN_DATA, module=('sklearn.pipeline', 'fit', 'Train Data'),
-                code_reference=CodeReference(lineno=24, col_offset=18), description=None): [
+                code_reference=CodeReference(24, 18, 26, 51), description=None): [
                     AnalyzerInputRow(
                         values=[46, 'Private', 128645, 'Some-college', 10, 'Divorced', 'Prof-specialty',
                                 'Not-in-family', 'White', 'Female', 0, 0, 40, 'United-States', '<=50K', 1],
@@ -92,39 +92,39 @@ def get_expected_result():
                                 'occupation', 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss',
                                 'hours-per-week', 'native-country', 'income-per-year', 'mlinspect_index'])],
         DagNode(node_id=56, operator_type=OperatorType.TRAIN_LABELS, module=('sklearn.pipeline', 'fit', 'Train Labels'),
-                code_reference=CodeReference(lineno=24, col_offset=18), description=None): [
+                code_reference=CodeReference(24, 18, 26, 51), description=None): [
                     AnalyzerInputRow(values=[array(1)], fields=['array']),
                     AnalyzerInputRow(values=[array(1)], fields=['array'])],
-        DagNode(node_id=40, operator_type=OperatorType.PROJECTION, code_reference=CodeReference(18, 25),
+        DagNode(node_id=40, operator_type=OperatorType.PROJECTION, code_reference=CodeReference(18, 25, 21, 2),
                 module=('sklearn.compose._column_transformer', 'ColumnTransformer', 'Projection'),
                 description="to ['age']"): [
                     AnalyzerInputRow(values=[46], fields=['age']), AnalyzerInputRow(values=[29], fields=['age'])],
-        DagNode(node_id=34, operator_type=OperatorType.PROJECTION, code_reference=CodeReference(18, 25),
+        DagNode(node_id=34, operator_type=OperatorType.PROJECTION, code_reference=CodeReference(18, 25, 21, 2),
                 module=('sklearn.compose._column_transformer', 'ColumnTransformer', 'Projection'),
                 description="to ['education']"): [
                     AnalyzerInputRow(values=['Some-college'], fields=['education']),
                     AnalyzerInputRow(values=['Some-college'], fields=['education'])],
-        DagNode(node_id=41, operator_type=OperatorType.PROJECTION, code_reference=CodeReference(18, 25),
+        DagNode(node_id=41, operator_type=OperatorType.PROJECTION, code_reference=CodeReference(18, 25, 21, 2),
                 module=('sklearn.compose._column_transformer', 'ColumnTransformer', 'Projection'),
                 description="to ['hours-per-week']"): [
                     AnalyzerInputRow(values=[40], fields=['hours-per-week']),
                     AnalyzerInputRow(values=[50], fields=['hours-per-week'])],
-        DagNode(node_id=35, operator_type=OperatorType.PROJECTION, code_reference=CodeReference(18, 25),
+        DagNode(node_id=35, operator_type=OperatorType.PROJECTION, code_reference=CodeReference(18, 25, 21, 2),
                 module=('sklearn.compose._column_transformer', 'ColumnTransformer', 'Projection'),
                 description="to ['workclass']"): [
                     AnalyzerInputRow(values=['Private'], fields=['workclass']),
                     AnalyzerInputRow(values=['Local-gov'], fields=['workclass'])],
-        DagNode(node_id=40, operator_type=OperatorType.TRANSFORMER, code_reference=CodeReference(20, 16),
+        DagNode(node_id=40, operator_type=OperatorType.TRANSFORMER, code_reference=CodeReference(20, 16, 20, 46),
                 module=('sklearn.preprocessing._data', 'StandardScaler', 'Pipeline'),
                 description="Numerical Encoder (StandardScaler), Column: 'age'"): [
                     AnalyzerInputRow(values=[array(RangeComparison(0.5, 0.6))], fields=['array']),
                     AnalyzerInputRow(values=[array(RangeComparison(-0.8, -0.7))], fields=['array'])],
-        DagNode(node_id=41, operator_type=OperatorType.TRANSFORMER, code_reference=CodeReference(20, 16),
+        DagNode(node_id=41, operator_type=OperatorType.TRANSFORMER, code_reference=CodeReference(20, 16, 20, 46),
                 module=('sklearn.preprocessing._data', 'StandardScaler', 'Pipeline'),
                 description="Numerical Encoder (StandardScaler), Column: 'hours-per-week'"): [
                     AnalyzerInputRow(values=[array(RangeComparison(-0.09, -0.08))], fields=['array']),
                     AnalyzerInputRow(values=[array(RangeComparison(0.7, 0.8))], fields=['array'])],
-        DagNode(node_id=34, operator_type=OperatorType.TRANSFORMER, code_reference=CodeReference(19, 20),
+        DagNode(node_id=34, operator_type=OperatorType.TRANSFORMER, code_reference=CodeReference(19, 20, 19, 72),
                 module=('sklearn.preprocessing._encoders', 'OneHotEncoder', 'Pipeline'),
                 description="Categorical Encoder (OneHotEncoder), Column: 'education'"): [
                     AnalyzerInputRow(
@@ -133,12 +133,12 @@ def get_expected_result():
                     AnalyzerInputRow(
                         values=[array([0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 1.])],
                         fields=['array'])],
-        DagNode(node_id=35, operator_type=OperatorType.TRANSFORMER, code_reference=CodeReference(19, 20),
+        DagNode(node_id=35, operator_type=OperatorType.TRANSFORMER, code_reference=CodeReference(19, 20, 19, 72),
                 module=('sklearn.preprocessing._encoders', 'OneHotEncoder', 'Pipeline'),
                 description="Categorical Encoder (OneHotEncoder), Column: 'workclass'"): [
                     AnalyzerInputRow(values=[array([0., 0., 1., 0., 0., 0., 0.])], fields=['array']),
                     AnalyzerInputRow(values=[array([0., 1., 0., 0., 0., 0., 0.])], fields=['array'])],
-        DagNode(node_id=46, operator_type=OperatorType.CONCATENATION, code_reference=CodeReference(18, 25),
+        DagNode(node_id=46, operator_type=OperatorType.CONCATENATION, code_reference=CodeReference(18, 25, 21, 2),
                 module=('sklearn.compose._column_transformer', 'ColumnTransformer', 'Concatenation'),
                 description=None): [
                     AnalyzerInputRow(values=[array([0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
@@ -151,7 +151,7 @@ def get_expected_result():
                                                     0., 0., 0., RangeComparison(-0.8, -0.7),
                                                     RangeComparison(0.7, 0.8)])],
                                      fields=['array'])],
-        DagNode(node_id=51, operator_type=OperatorType.ESTIMATOR, code_reference=CodeReference(26, 19),
+        DagNode(node_id=51, operator_type=OperatorType.ESTIMATOR, code_reference=CodeReference(26, 19, 26, 48),
                 module=('sklearn.tree._classes', 'DecisionTreeClassifier', 'Pipeline'),
                 description='Decision Tree'): None
     }
