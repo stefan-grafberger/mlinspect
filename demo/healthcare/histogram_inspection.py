f2018b5ac6c21a8225c13d8fda1be76636131a7f
stefan-grafberger <stefangrafberger@gmail.com>
2020-09-28 22:33:21 +0200

    Everything works again with new inspection input format. Still some polish todos

--- demo/healthcare/histogram_inspection.py
+++ demo/healthcare/histogram_inspection.py
@@ -22,7 +22,7 @@ class HistogramInspection(Inspection):
         """
         Visit an operator
         """
-        # pylint: disable=too-many-branches, too-many-statements
+        # pylint: disable=too-many-branches, too-many-statements, too-many-locals
         current_count = - 1
 
         age_group_map = {}
@@ -34,41 +34,69 @@ class HistogramInspection(Inspection):
             age_group = [None, None]
             if inspection_input.operator_context.function_info == ('sklearn.impute._base', 'fit_transform') and \
                     "age_group" in inspection_input.input_columns.fields:
-                age_group_index = inspection_input.input_columns.get_index_of_column("age_group")
+                age_group_input_index = 0
+                age_group_output_index = inspection_input.input_columns.get_index_of_column("age_group")
+                age_group_input = 1
                 age_group_output = 0
                 age_group_annotation = 1
-            elif "age_group" in inspection_input.output_columns.fields:
-                age_group_index = inspection_input.output_columns.get_index_of_column("age_group")
-                age_group_output = 0
+            elif "age_group" in inspection_input.input_columns.fields:
+                age_group_input_index = inspection_input.input_columns.get_index_of_column("age_group")
+                age_group_output_index = 0
+                age_group_input = 0
+                age_group_output = 1
                 age_group_annotation = 1
             else:
-                age_group_index = 0
+                age_group_input_index = 0
+                age_group_output_index = 0
+                age_group_input = 1
                 age_group_output = 1
                 age_group_annotation = 0
             race = [None, None]
             if inspection_input.operator_context.function_info == ('sklearn.impute._base', 'fit_transform') and \
                     "race" in inspection_input.input_columns.fields:
-                race_index = inspection_input.input_columns.get_index_of_column("race")
+                race_input_index = 0
+                race_output_index = inspection_input.input_columns.get_index_of_column("race")
+                race_input = 1
                 race_output = 0
                 race_annotation = 1
-            elif "race" in inspection_input.output_columns.fields:
-                race_index = inspection_input.output_columns.get_index_of_column("race")
-                race_output = 0
+            elif "race" in inspection_input.input_columns.fields:
+                race_input_index = inspection_input.input_columns.get_index_of_column("race")
+                race_output_index = 0
+                race_input = 0
+                race_output = 1
                 race_annotation = 1
             else:
-                race_index = 0
+                race_output_index = 0
+                race_input_index = 0
+                race_input = 1
                 race_output = 1
                 race_annotation = 0
-            for row in inspection_input.row_iterator:
-                current_count += 1
-                age_group[age_group_output] = row.input[age_group_index]
-                age_group[age_group_annotation] = row.annotation[0]
-                race[race_output] = row.input[race_index]
-                race[race_annotation] = row.annotation[1]
+            if inspection_input.operator_context.operator != OperatorType.TRANSFORMER:
+                for row in inspection_input.row_iterator:
+                    current_count += 1
+                    age_group[age_group_input] = row.input[age_group_input_index]
+                    age_group[age_group_annotation] = row.annotation[0]
+                    race[race_input] = row.input[race_input_index]
+                    race[race_annotation] = row.annotation[1]
 
-                annotation = (age_group[0], race[0])
-                self.update_histogram_maps(age_group[0], age_group_map, race[0], race_count_map)
-                yield annotation
+                    annotation = (age_group[0], race[0])
+                    self.update_histogram_map(age_group[0], age_group_map)
+                    self.update_histogram_map(race[0], race_count_map)
+                    yield annotation
+            else:
+                for row in inspection_input.row_iterator:
+                    current_count += 1
+                    age_group[age_group_input] = row.input[age_group_input_index]
+                    age_group[age_group_output] = row.output[age_group_output_index][0]
+                    age_group[age_group_annotation] = row.annotation[0]
+                    race[race_input] = row.input[race_input_index]
+                    race[race_output] = row.output[race_output_index][0]
+                    race[race_annotation] = row.annotation[1]
+
+                    annotation = (age_group[0], race[0])
+                    self.update_histogram_map(age_group[0], age_group_map)
+                    self.update_histogram_map(race[0], race_count_map)
+                    yield annotation
         elif isinstance(inspection_input, InspectionInputDataSource):
             if "age_group" in inspection_input.output_columns.fields:
                 age_group_index = inspection_input.output_columns.get_index_of_column("age_group")
@@ -76,7 +104,7 @@ class HistogramInspection(Inspection):
             else:
                 age_group_present = False
             if "race" in inspection_input.output_columns.fields:
-                race_index = inspection_input.output_columns.get_index_of_column("age_group")
+                race_index = inspection_input.output_columns.get_index_of_column("race")
                 race_present = True
             else:
                 race_present = False
@@ -84,14 +112,10 @@ class HistogramInspection(Inspection):
                 current_count += 1
                 if age_group_present:
                     age_group = row.output[age_group_index]
-                    group_count = age_group_map.get(age_group, 0)
-                    group_count += 1
-                    age_group_map[age_group] = group_count
+                    self.update_histogram_map(age_group, age_group_map)
                 if race_present:
-                    race_group = row.output[race_index]
-                    group_count = race_count_map.get(race_group, 0)
-                    group_count += 1
-                    race_count_map[race_group] = group_count
+                    race = row.output[race_index]
+                    self.update_histogram_map(race, race_count_map)
                 yield None
         elif isinstance(inspection_input, InspectionInputNAryOperator):
             if "age_group" in inspection_input.output_columns.fields:
@@ -100,7 +124,7 @@ class HistogramInspection(Inspection):
             else:
                 age_group_present = False
             if "race" in inspection_input.output_columns.fields:
-                race_index = inspection_input.output_columns.get_index_of_column("age_group")
+                race_index = inspection_input.output_columns.get_index_of_column("race")
                 race_present = True
             else:
                 race_present = False
@@ -110,10 +134,12 @@ class HistogramInspection(Inspection):
                     age_group = row.output[age_group_index]
                 else:
                     age_group = row.annotation[0][0]
+                self.update_histogram_map(age_group, age_group_map)
                 if race_present:
                     race = row.output[race_index]
                 else:
                     race = row.annotation[0][1]
+                self.update_histogram_map(race, race_count_map)
                 yield age_group, race
         else:
             for _ in inspection_input.row_iterator:
@@ -122,16 +148,13 @@ class HistogramInspection(Inspection):
         self._histogram_op_output = {"age_group_counts": age_group_map, "race_counts": race_count_map}
 
     @staticmethod
-    def update_histogram_maps(age_group, age_group_map, race, race_count_map):
+    def update_histogram_map(group, histogram_map):
         """
         Update the histogram maps.
         """
-        group_count = age_group_map.get(age_group, 0)
+        group_count = histogram_map.get(group, 0)
         group_count += 1
-        age_group_map[age_group] = group_count
-        group_count = race_count_map.get(race, 0)
-        group_count += 1
-        race_count_map[race] = group_count
+        histogram_map[group] = group_count
 
     def get_operator_annotation_after_visit(self) -> any:
         assert self._operator_type
