942d107cbed4c41b06dbad664045ff7e60cb9101
stefan-grafberger <stefangrafberger@gmail.com>
2020-09-29 13:59:37 +0200

    Removed code that is no longer needed after the backend refactoring

--- mlinspect/backends/sklearn_backend_transformer_wrapper.py
+++ mlinspect/backends/sklearn_backend_transformer_wrapper.py
@@ -21,10 +21,10 @@ from ..instrumentation.dag_node import CodeReference, OperatorType
 transformer_names = {
         ('sklearn.preprocessing._encoders', 'OneHotEncoder'): "Categorical Encoder (OneHotEncoder)",
         ('sklearn.preprocessing._data', 'StandardScaler'): "Numerical Encoder (StandardScaler)",
-        ('demo.healthcare.demo_utils', 'MyW2VTransformer'): "Word2Vec",  # FIXME
-        ('sklearn.demo.healthcare.demo_utils', 'MyW2VTransformer'): "Word2Vec",
         ('sklearn.impute._base', 'SimpleImputer'): "Imputer (SimpleImputer)",
         ('sklearn.tree._classes', 'DecisionTreeClassifier'): "Decision Tree",
+        # TODO: We  can remove this later by checking if subclass of transformer/estimator
+        ('demo.healthcare.demo_utils', 'MyW2VTransformer'): "Word2Vec",
         ('tensorflow.python.keras.wrappers.scikit_learn', 'KerasClassifier'): "Neural Network"
     }
 
