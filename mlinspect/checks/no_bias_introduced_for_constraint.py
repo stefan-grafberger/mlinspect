705f45a221b9b9852c454ba292a0647df49ca2aa
stefan-grafberger <stefangrafberger@gmail.com>
2020-10-01 20:47:15 +0200

    WIP: no_bias_introduced_for_constraint

--- mlinspect/checks/no_bias_introduced_for_constraint.py
+++ mlinspect/checks/no_bias_introduced_for_constraint.py
@@ -3,12 +3,24 @@ The Interface for the Constraints
 """
 from __future__ import annotations
 
-from typing import Iterable
+import dataclasses
+from typing import Iterable, List, Tuple
+
+from werkzeug.routing import Map
 
 from mlinspect.checks.constraint import Constraint, ConstraintStatus, ConstraintResult
 from mlinspect.inspections.histogram_inspection import HistogramInspection
 from mlinspect.inspections.inspection import Inspection
+from mlinspect.instrumentation.dag_node import OperatorType
 from mlinspect.instrumentation.inspection_result import InspectionResult
+
+
+@dataclasses.dataclass
+class NoBiasIntroducedforConstraintResult(ConstraintResult):
+    """
+    Does this check cause an error or a warning if it fails?
+    """
+    histograms_after_before: List[Tuple[Map]]
 
 
 class NoBiasIntroducedForConstraint(Constraint):
@@ -27,5 +39,16 @@ class NoBiasIntroducedForConstraint(Constraint):
 
     def evaluate(self, inspection_result: InspectionResult) -> ConstraintResult:
         """Evaluate the constraint"""
-        # TODO
-        return ConstraintResult(self, ConstraintStatus.SUCCESS)
+        dag = inspection_result.dag
+        histograms = inspection_result.inspection_to_annotations[HistogramInspection(self.sensitive_columns)]
+        relevant_nodes = [node for node in dag.nodes if node.operator_type in {OperatorType.JOIN,
+                                                                               OperatorType.SELECTION} or
+                          node.module == ('sklearn.impute._base', 'fit_transform')]
+        result = ConstraintStatus.SUCCESS
+        histograms_after_before = []
+        for node in relevant_nodes:
+            parents = dag.predecessors(node)
+            # TODO
+            print(histograms)
+            print(parents)
+        return NoBiasIntroducedforConstraintResult(self, result, histograms_after_before)
