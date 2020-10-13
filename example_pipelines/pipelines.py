ce7f462c7349183aceab32f54e9ddf0e95abad2e
stefan-grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2020-10-13 14:51:43 +0200

    Added compas pipeline code. TODO: Add support for pandas.replace

--- example_pipelines/pipelines.py
+++ example_pipelines/pipelines.py
@@ -10,4 +10,6 @@ ADULT_EASY_IPYNB = os.path.join(str(get_project_root()), "example_pipelines", "a
 
 ADULT_NORMAL_PY = os.path.join(str(get_project_root()), "example_pipelines", "adult_normal", "adult_normal.py")
 
+COMPAS_PY = os.path.join(str(get_project_root()), "example_pipelines", "compas", "compas.py")
+
 HEALTHCARE_PY = os.path.join(str(get_project_root()), "example_pipelines", "healthcare", "healthcare.py")
