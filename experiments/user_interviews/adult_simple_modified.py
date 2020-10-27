f2e121a7fc94563a393a687b1db272aea7fe1663
stefan-grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2020-10-27 14:37:47 +0100

    Trying to fix duplicate code issue. Sadly, pyling doesnt allow partial disable

--- experiments/user_interviews/adult_simple_modified.py
+++ experiments/user_interviews/adult_simple_modified.py
@@ -15,23 +15,21 @@ raw_data_a = pd.read_csv(train_file_a, na_values='?', index_col=0)
 train_file_b = os.path.join(str(get_project_root()), "experiments", "user_interviews", "adult_simple_train_b.csv")
 raw_data_b = pd.read_csv(train_file_b, na_values='?', index_col=0)
 
-raw_data = raw_data_a.merge(raw_data_b, on="id")
+merged_raw_data = raw_data_a.merge(raw_data_b, on="id")
 
-data = raw_data.dropna()
+data = merged_raw_data.dropna()
 
 labels = preprocessing.label_binarize(data['income-per-year'], classes=['>50K', '<=50K'])
 
-feature_transformation = compose.ColumnTransformer(transformers=[
+column_transformer = compose.ColumnTransformer(transformers=[
     ('categorical', preprocessing.OneHotEncoder(handle_unknown='ignore'), ['education', 'workclass']),
     ('numeric', preprocessing.StandardScaler(), ['age', 'hours-per-week'])
 ])
 
 
-income_pipeline = pipeline.Pipeline([
-    ('features', feature_transformation),
+adult_income_pipeline = pipeline.Pipeline([
+    ('features', column_transformer),
     ('classifier', tree.DecisionTreeClassifier())])
 
-income_pipeline.fit(data, labels)
-
-
+adult_income_pipeline.fit(data, labels)
 print('pipeline finished')
--- experiments/user_interviews/compas_modified.py
+++ experiments/user_interviews/compas_modified.py
@@ -13,49 +13,49 @@ from sklearn.preprocessing import OneHotEncoder, KBinsDiscretizer, label_binariz
 from mlinspect.utils import get_project_root
 
 train_file = os.path.join(str(get_project_root()), "experiments", "user_interviews", "compas_train_modified.csv")
-train_data = pd.read_csv(train_file, na_values='?', index_col=0)
+mod_train = pd.read_csv(train_file, na_values='?', index_col=0)
 test_file = os.path.join(str(get_project_root()), "example_pipelines", "compas", "compas_test.csv")
-test_data = pd.read_csv(test_file, na_values='?', index_col=0)
+test = pd.read_csv(test_file, na_values='?', index_col=0)
 
-train_data = train_data[
+mod_train = mod_train[
     ['sex', 'dob', 'age', 'c_charge_degree', 'race', 'score_text', 'priors_count', 'days_b_screening_arrest',
      'decile_score', 'is_recid', 'two_year_recid', 'c_jail_in', 'c_jail_out']]
-test_data = test_data[
+test = test[
     ['sex', 'dob', 'age', 'c_charge_degree', 'race', 'score_text', 'priors_count', 'days_b_screening_arrest',
      'decile_score', 'is_recid', 'two_year_recid', 'c_jail_in', 'c_jail_out']]
 
 # If the charge date of a defendants Compas scored crime was not within 30 days from when the person was arrested,
 # we assume that because of data quality reasons, that we do not have the right offense.
-train_data = train_data[(train_data['days_b_screening_arrest'] <= 30) & (train_data['days_b_screening_arrest'] >= -30)]
+mod_train = mod_train[(mod_train['days_b_screening_arrest'] <= 30) & (mod_train['days_b_screening_arrest'] >= -30)]
 # We coded the recidivist flag – is_recid – to be -1 if we could not find a compas case at all.
-train_data = train_data[train_data['is_recid'] != -1]
+mod_train = mod_train[mod_train['is_recid'] != -1]
 # In a similar vein, ordinary traffic offenses – those with a c_charge_degree of ‘O’ – will not result in Jail
 # time are removed (only two of them).
-train_data = train_data[train_data['c_charge_degree'] != "O"]
+mod_train = mod_train[mod_train['c_charge_degree'] != "O"]
 # We filtered the underlying data from Broward county to include only those rows representing people who had either
 # recidivated in two years, or had at least two years outside of a correctional facility.
-train_data = train_data[train_data['score_text'] != 'N/A']
+mod_train = mod_train[mod_train['score_text'] != 'N/A']
 
-train_data = train_data.replace('Medium', "Low")
-test_data = test_data.replace('Medium', "Low")
+mod_train = mod_train.replace('Medium', "Low")
+test = test.replace('Medium', "Low")
 
-train_labels = label_binarize(train_data['score_text'], classes=['High', 'Low'])
-test_labels = label_binarize(test_data['score_text'], classes=['High', 'Low'])
+train_labels = label_binarize(mod_train['score_text'], classes=['High', 'Low'])
+test_labels = label_binarize(test['score_text'], classes=['High', 'Low'])
 
-impute1_and_onehot = Pipeline([('imputer1', SimpleImputer(strategy='most_frequent')),
-                               ('onehot', OneHotEncoder(handle_unknown='ignore'))])
-impute2_and_bin = Pipeline([('imputer2', SimpleImputer(strategy='mean')),
-                            ('discretizer', KBinsDiscretizer(n_bins=4, encode='ordinal', strategy='uniform'))])
+impute_and_onehot = Pipeline([('imputer1', SimpleImputer(strategy='most_frequent')),
+                              ('onehot', OneHotEncoder(handle_unknown='ignore'))])
+impute_and_bin = Pipeline([('imputer2', SimpleImputer(strategy='mean')),
+                           ('discretizer', KBinsDiscretizer(n_bins=4, encode='ordinal', strategy='uniform'))])
 
-featurizer = ColumnTransformer(transformers=[
-    ('impute1_and_onehot', impute1_and_onehot, ['is_recid']),
-    ('impute2_and_bin', impute2_and_bin, ['age'])
+compas_featurizer = ColumnTransformer(transformers=[
+    ('impute1_and_onehot', impute_and_onehot, ['is_recid']),
+    ('impute2_and_bin', impute_and_bin, ['age'])
 ])
 
-pipeline = Pipeline([
-    ('features', featurizer),
+compas_pipeline = Pipeline([
+    ('features', compas_featurizer),
     ('classifier', LogisticRegression())
 ])
 
-pipeline.fit(train_data, train_labels.ravel())
-print(pipeline.score(test_data, test_labels.ravel()))
+compas_pipeline.fit(mod_train, train_labels.ravel())
+print(compas_pipeline.score(test, test_labels.ravel()))
