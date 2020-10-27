5a709c9eaa4b3b67baaf922a2c487f824df6c750
stefan-grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2020-10-27 13:40:42 +0100

    Removed code used to introduce artifical issue

--- experiments/user_interviews/adult_simple_modified.py
+++ experiments/user_interviews/adult_simple_modified.py
@@ -9,7 +9,7 @@ from mlinspect.utils import get_project_root
 
 print('pipeline start')
 
-train_file_a =os.path.join(str(get_project_root()), "experiments", "user_interviews", "adult_simple_train_a.csv")
+train_file_a = os.path.join(str(get_project_root()), "experiments", "user_interviews", "adult_simple_train_a.csv")
 raw_data_a = pd.read_csv(train_file_a, na_values='?', index_col=0)
 
 train_file_b = os.path.join(str(get_project_root()), "experiments", "user_interviews", "adult_simple_train_b.csv")
