22a1dce79226c4ac673b3032454a67ff07a274a8
stefan-grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2021-01-11 16:40:55 +0100

    Marking lines with enter key is working

--- assets/test.js
+++ assets/test.js
@@ -10,6 +10,14 @@ setTimeout(function(){
             // can use other themes, only need to include css file from https://cdnjs.com/libraries/codemirror
             // see https://codemirror.net/demo/theme.html#default
         });
+
+        document.addEventListener("keydown", keyDownTextField, false);
+        function keyDownTextField(e) {
+        var keyCode = e.keyCode;
+          if(keyCode==13) { // Enter key
+            editor.markText({line:1,ch:0},{line:10,ch:0},{className:  "code-dag-highlight"});
+          }
+        }
     });
 
 }, 2000);
