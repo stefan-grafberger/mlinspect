9800512b28cdc23a0a743fd71107c8b0b4d4a858
stefan-grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2021-01-11 22:54:24 +0100

    Hovering is mostly working. Not for some operators in line 37. Hover end not registered properly always

--- assets/test.js
+++ assets/test.js
@@ -1,38 +1,50 @@
+function hightightCodeBlock(editor, codeReferenceText){
+    code_reference = JSON.parse(codeReferenceText);
+    console.log(code_reference); // todo: call marktext
+    document.querySelectorAll('.code-dag-highlight').forEach(block => {
+        block.classList.remove("code-dag-highlight");
+    });
+    editor.markText(
+        {line:(code_reference.lineno-1), ch:code_reference.col_offset},
+        {line:(code_reference.end_lineno-1), ch:code_reference.end_col_offset},
+        {className:  "code-dag-highlight"}
+    );
+}
+
 setTimeout(function(){
-   document.querySelectorAll('#pipeline-textarea').forEach(block => {
-
-        // then highlight each
-        var editor = CodeMirror.fromTextArea(block, {
-            lineNumbers: true,
-            matchBrackets: true,
-            mode: "text/x-python",
-            theme : "default"
-            // can use other themes, only need to include css file from https://cdnjs.com/libraries/codemirror
-            // see https://codemirror.net/demo/theme.html#default
-        });
-
-        document.addEventListener("keydown", keyDownTextField, false);
-        function keyDownTextField(e) {
-        var keyCode = e.keyCode;
-          if(keyCode==13) { // Enter key
-            editor.markText({line:1,ch:0},{line:10,ch:0},{className:  "code-dag-highlight"});
-          }
-        }
-
-        //More Details https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
-        // select the target node
-        var target = document.querySelector('#pipeline-textarea') // todo: use hidden div
-        console.log("attributes: ");
-        console.log(target.attributes);
-        console.log(target.getAttribute('n_clicks_timestamp')); // todo: get actual attributes
-        // create an observer instance
-        var observer = new MutationObserver(function(mutations) {
-          console.log(target.innerText); // todo: call marktext
-        });
-        // configuration of the observer:
-        var config = { attributes: true, childList: true, characterData: true };
-        // pass in the target node, as well as the observer options
-        observer.observe(target, config);
+   textArea = document.querySelector('#pipeline-textarea')
+    console.log(textArea);
+    // then highlight each
+    var editor = CodeMirror.fromTextArea(textArea, {
+        lineNumbers: true,
+        matchBrackets: true,
+        mode: "text/x-python",
+        theme : "default"
+        // can use other themes, only need to include css file from https://cdnjs.com/libraries/codemirror
+        // see https://codemirror.net/demo/theme.html#default
     });
 
+    //More Details https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
+    // select the target node
+    var hoverDiv = document.querySelector('#hovered-code-reference');
+    var selectDiv = document.querySelector('#selected-code-reference');
+    //console.log(target.getAttribute('n_clicks_timestamp')); // todo: get actual attributes
+    // create an observer instance
+    var observer = new MutationObserver(function(mutations) {
+        console.log("attributes: ");
+        if (hoverDiv.innerText) {
+            hightightCodeBlock(editor, hoverDiv.innerText);
+        } else if (selectDiv.innerText) {
+            hightightCodeBlock(editor, selectDiv.innerText);
+        } else {
+            document.querySelectorAll('.code-dag-highlight').forEach(block => {
+                block.classList.remove("code-dag-highlight");
+            });
+        }
+    });
+    // configuration of the observer:
+    var config = { attributes: true, childList: true, characterData: true };
+    // pass in the target node, as well as the observer options
+    observer.observe(hoverDiv, config);
+    observer.observe(selectDiv, config);
 }, 2000);
