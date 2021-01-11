94b2afacbd379fa3ff4d3a0d4e645a7727faad46
stefan-grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2021-01-11 20:21:11 +0100

    Preparing usage of hidden div to highlight code

--- assets/test.js
+++ assets/test.js
@@ -18,6 +18,21 @@ setTimeout(function(){
             editor.markText({line:1,ch:0},{line:10,ch:0},{className:  "code-dag-highlight"});
           }
         }
+
+        //More Details https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
+        // select the target node
+        var target = document.querySelector('#pipeline-textarea') // todo: use hidden div
+        console.log("attributes: ");
+        console.log(target.attributes);
+        console.log(target.getAttribute('n_clicks_timestamp')); // todo: get actual attributes
+        // create an observer instance
+        var observer = new MutationObserver(function(mutations) {
+          console.log(target.innerText); // todo: call marktext
+        });
+        // configuration of the observer:
+        var config = { attributes: true, childList: true, characterData: true };
+        // pass in the target node, as well as the observer options
+        observer.observe(target, config);
     });
 
 }, 2000);
