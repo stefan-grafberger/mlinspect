19709f0965621003af769b2d51f4d1c887e84205
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2021-03-02 19:20:36 +0100

    Datawig pipeline

--- example_pipelines/healthcare/healthcare.py
+++ example_pipelines/healthcare/healthcare.py
@@ -2,11 +2,24 @@
 An example pipeline
 """
 import os
+import warnings
 
 import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
+from example_pipelines.healthcare.healthcare_utils import MyW2VTransformer, create_model
+from mlinspect.utils import get_project_root
 from datawig import SimpleImputer as DatawigSimpleImputer
 
-from mlinspect.utils import get_project_root
+
+# FutureWarning: Given feature/column names or counts do not match the ones for the data given during fit
+warnings.filterwarnings('ignore')
+
+COUNTIES_OF_INTEREST = ['county2', 'county3']
 
 # load input data sources (data generated with https://www.mockaroo.com as a single file and then split into two)
 patients = pd.read_csv(os.path.join(str(get_project_root()), "example_pipelines", "healthcare",
@@ -17,11 +30,16 @@ histories = pd.read_csv(os.path.join(str(get_project_root()), "example_pipelines
 # combine input data into a single table
 data = patients.merge(histories, on=['ssn'])
 
-# project data to a subset of attributes
-data = data[['smoker', 'last_name', 'county', 'num_children', 'race', 'income']]
+# compute mean complications per age group, append as column
+complications = data.groupby('age_group').agg(mean_complications=('complications', 'mean'))
 
-# we have rows with missing values now
-print(data[data.isnull().any(axis=1)])
+data = data.merge(complications, on=['age_group'])
+
+# target variable: people with a high number of complications
+data['label'] = data['complications'] > 1.2 * data['mean_complications']
+
+# project data to a subset of attributes
+data = data[['smoker', 'last_name', 'county', 'num_children', 'race', 'income', 'label']]
 
 # imputing
 smoker_imputer = DatawigSimpleImputer(
@@ -51,7 +69,27 @@ county_imputer.fit(train_df=data, num_epochs=5)
 data = county_imputer.predict(data)
 data["county"] = data["county_imputed"]
 
-data = data[['smoker', 'last_name', 'county', 'num_children', 'race', 'income']]
+data = data[['smoker', 'last_name', 'county', 'num_children', 'race', 'income', 'label']]
 
-# no rows should now have missing values
-#print(data[data.isnull().any(axis=1)])
+# filter data
+data = data[data['county'].isin(COUNTIES_OF_INTEREST)]
+
+# define the feature encoding of the data
+featurisation = ColumnTransformer(transformers=[
+    ("impute_and_one_hot_encode", OneHotEncoder(sparse=False, handle_unknown='ignore'), ['smoker', 'county', 'race']),
+    ('word2vec', MyW2VTransformer(min_count=2), ['last_name']),
+    ('numeric', StandardScaler(), ['num_children', 'income'])
+])
+
+# define the training pipeline for the model
+neural_net = KerasClassifier(build_fn=create_model, epochs=10, batch_size=1, verbose=0, input_dim=108)
+pipeline = Pipeline([
+    ('features', featurisation),
+    ('learner', neural_net)])
+
+# train-test split
+train_data, test_data = train_test_split(data, random_state=0)
+# model training
+model = pipeline.fit(train_data, train_data['label'])
+# model evaluation
+print(model.score(test_data, test_data['label']))
