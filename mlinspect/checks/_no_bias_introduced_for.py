76aba864d8764999df5427a53f64cc5efba6cbe5
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2021-05-11 14:14:43 +0200

    Minor wording clarification

--- mlinspect/checks/_no_bias_introduced_for.py
+++ mlinspect/checks/_no_bias_introduced_for.py
@@ -202,7 +202,7 @@ class NoBiasIntroducedFor(Check):
         removal_probabilities = distribution_change.before_and_after_df["normed_removal_probability"]
 
         pyplot.bar(keys, removal_probabilities)
-        pyplot.gca().set_title("normed removal probability per sensitive group")
+        pyplot.gca().set_title("normed removal probability per member of sensitive group")
         pyplot.xticks(
             rotation=45,
             horizontalalignment='right',
