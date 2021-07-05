f695366534d43569a0e48adb16c629d250a7189c
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2021-07-05 11:32:24 +0200

    IntersectionalHistogramForColumns

---
+++ mlinspect/inspections/_intersectional_histogram_for_columns.py
@@ -0,0 +1,128 @@
+"""
+A simple example inspection
+"""
+from typing import Iterable, List
+
+from mlinspect.inspections._inspection import Inspection
+from mlinspect.inspections._inspection_input import InspectionInputDataSource, \
+    InspectionInputUnaryOperator, InspectionInputNAryOperator, OperatorType, FunctionInfo
+
+
+class IntersectionalHistogramForColumns(Inspection):
+    """
+    A simple example inspection
+    """
+
+    def __init__(self, sensitive_columns: List[str]):
+        self._histogram_op_output = None
+        self._operator_type = None
+        self.sensitive_columns = sensitive_columns
+
+    @property
+    def inspection_id(self):
+        return tuple(self.sensitive_columns)
+
+    def visit_operator(self, inspection_input) -> Iterable[any]:
+        """
+        Visit an operator
+        """
+        # pylint: disable=too-many-branches, too-many-statements, too-many-locals
+        current_count = - 1
+
+        histogram_map = {}
+
+        self._operator_type = inspection_input.operator_context.operator
+
+        if isinstance(inspection_input, InspectionInputUnaryOperator):
+            sensitive_columns_present = []
+            sensitive_columns_index = []
+            for column in self.sensitive_columns:
+                column_present = column in inspection_input.input_columns.fields
+                sensitive_columns_present.append(column_present)
+                column_index = inspection_input.input_columns.get_index_of_column(column)
+                sensitive_columns_index.append(column_index)
+            if inspection_input.operator_context.function_info == FunctionInfo('sklearn.impute._base', 'SimpleImputer'):
+                for row in inspection_input.row_iterator:
+                    current_count += 1
+                    column_values = []
+                    for check_index, _ in enumerate(self.sensitive_columns):
+                        if sensitive_columns_present[check_index]:
+                            column_value = row.output[0][sensitive_columns_index[check_index]]
+                        else:
+                            column_value = row.annotation[check_index]
+                        column_values.append(column_value)
+                    update_histograms(column_values, histogram_map)
+                    yield column_values
+            else:
+                for row in inspection_input.row_iterator:
+                    current_count += 1
+                    column_values = []
+                    for check_index, _ in enumerate(self.sensitive_columns):
+                        if sensitive_columns_present[check_index]:
+                            column_value = row.input[sensitive_columns_index[check_index]]
+                        else:
+                            column_value = row.annotation[check_index]
+                        column_values.append(column_value)
+                    update_histograms(column_values, histogram_map)
+                    yield column_values
+        elif isinstance(inspection_input, InspectionInputDataSource):
+            sensitive_columns_present = []
+            sensitive_columns_index = []
+            for column in self.sensitive_columns:
+                column_present = column in inspection_input.output_columns.fields
+                sensitive_columns_present.append(column_present)
+                column_index = inspection_input.output_columns.get_index_of_column(column)
+                sensitive_columns_index.append(column_index)
+            for row in inspection_input.row_iterator:
+                current_count += 1
+                column_values = []
+                for check_index, _ in enumerate(self.sensitive_columns):
+                    if sensitive_columns_present[check_index]:
+                        column_value = row.output[sensitive_columns_index[check_index]]
+                        column_values.append(column_value)
+                    else:
+                        column_values.append(None)
+                update_histograms(column_values, histogram_map)
+                yield column_values
+        elif isinstance(inspection_input, InspectionInputNAryOperator):
+            sensitive_columns_present = []
+            sensitive_columns_index = []
+            for column in self.sensitive_columns:
+                column_present = column in inspection_input.output_columns.fields
+                sensitive_columns_present.append(column_present)
+                column_index = inspection_input.output_columns.get_index_of_column(column)
+                sensitive_columns_index.append(column_index)
+            for row in inspection_input.row_iterator:
+                current_count += 1
+                column_values = []
+                for check_index, _ in enumerate(self.sensitive_columns):
+                    if sensitive_columns_present[check_index]:
+                        column_value = row.output[sensitive_columns_index[check_index]]
+                        column_values.append(column_value)
+                    else:
+                        column_values.append(None)
+                update_histograms(column_values, histogram_map)
+                yield column_values
+        else:
+            for _ in inspection_input.row_iterator:
+                yield None
+
+        self._histogram_op_output = histogram_map
+
+    def get_operator_annotation_after_visit(self) -> any:
+        assert self._operator_type
+        if self._operator_type is not OperatorType.ESTIMATOR:
+            result = self._histogram_op_output
+            self._histogram_op_output = None
+            self._operator_type = None
+            return result
+        self._operator_type = None
+        return None
+
+
+def update_histograms(column_values, histogram_map):
+    """Update the histograms with the intersectional information"""
+    value_tuple = tuple(column_values)
+    group_count = histogram_map.get(value_tuple, 0)
+    group_count += 1
+    histogram_map[value_tuple] = group_count
---
+++ test/inspections/test_intersectional_histogram_for_columns.py
@@ -0,0 +1,74 @@
+"""
+Tests whether HistogramForColumns works
+"""
+from inspect import cleandoc
+
+from testfixtures import compare
+
+from mlinspect._pipeline_inspector import PipelineInspector
+from mlinspect.inspections import IntersectionalHistogramForColumns
+
+
+def test_histogram_merge():
+    """
+    Tests whether RowLineage works for joins
+    """
+    test_code = cleandoc("""
+            import pandas as pd
+
+            df_a = pd.DataFrame({'A': ['cat_a', 'cat_b', 'cat_a', 'cat_b', 'cat_b'], 
+                'B': [1, 2, 4, 5, 7],
+                'C': [True, False, True, True, True]})
+            df_b = pd.DataFrame({'B': [1, 2, 3, 4, 5], 'D': [1, 5, 4, 11, None]})
+            df_merged = df_a.merge(df_b, on='B')
+            """)
+
+    inspector_result = PipelineInspector \
+        .on_pipeline_from_string(test_code) \
+        .add_required_inspection(IntersectionalHistogramForColumns(["A", "C"])) \
+        .execute()
+    inspection_results = list(inspector_result.dag_node_to_inspection_results.values())
+
+    histogram_output = inspection_results[0][IntersectionalHistogramForColumns(["A", "C"])]
+    expected_histogram = {('cat_a', True): 2, ('cat_b', False): 1, ('cat_b', True): 2}
+    compare(histogram_output, expected_histogram)
+
+    histogram_output = inspection_results[1][IntersectionalHistogramForColumns(["A", "C"])]
+    expected_histogram = {(None, None): 5}
+    compare(histogram_output, expected_histogram)
+
+    histogram_output = inspection_results[2][IntersectionalHistogramForColumns(["A", "C"])]
+    expected_histogram = {('cat_a', True): 2, ('cat_b', False): 1, ('cat_b', True): 1}
+    compare(histogram_output, expected_histogram)
+
+
+def test_histogram_projection():
+    """
+    Tests whether RowLineage works for joins
+    """
+    test_code = cleandoc("""
+            import pandas as pd
+
+            pandas_df = pd.DataFrame({'A': ['cat_a', 'cat_b', 'cat_a', 'cat_c', 'cat_b'], 
+                'B': [1, 2, 4, 5, 7], 'C': [True, False, True, True, True]})
+            pandas_df = pandas_df[['B', 'C']]
+            pandas_df = pandas_df[['C']]
+            """)
+
+    inspector_result = PipelineInspector \
+        .on_pipeline_from_string(test_code) \
+        .add_required_inspection(IntersectionalHistogramForColumns(["A", "C"])) \
+        .execute()
+    inspection_results = list(inspector_result.dag_node_to_inspection_results.values())
+
+    histogram_output = inspection_results[0][IntersectionalHistogramForColumns(["A", "C"])]
+    expected_histogram = {('cat_a', True): 2, ('cat_b', False): 1, ('cat_c', True): 1, ('cat_b', True): 1}
+    compare(histogram_output, expected_histogram)
+
+    histogram_output = inspection_results[1][IntersectionalHistogramForColumns(["A", "C"])]
+    expected_histogram = {('cat_a', True): 2, ('cat_b', False): 1, ('cat_c', True): 1, ('cat_b', True): 1}
+    compare(histogram_output, expected_histogram)
+
+    histogram_output = inspection_results[2][IntersectionalHistogramForColumns(["A", "C"])]
+    expected_histogram = {('cat_a', True): 2, ('cat_b', False): 1, ('cat_c', True): 1, ('cat_b', True): 1}
+    compare(histogram_output, expected_histogram)
