e0fd5ddb7e1f2443dc1cc4ad431d1ee7479475bb
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2021-07-08 18:30:42 +0200

    FunctionTransformer (Closes #65) (PR #66)

--- mlinspect/monkeypatching/_patch_sklearn.py
+++ mlinspect/monkeypatching/_patch_sklearn.py
@@ -633,6 +633,110 @@ class SklearnSimpleImputerPatching:
         return new_return_value
 
 
+@gorilla.patches(preprocessing.FunctionTransformer)
+class SklearnFunctionTransformerPatching:
+    """ Patches for sklearn FunctionTransformer"""
+
+    # pylint: disable=too-few-public-methods
+
+    @gorilla.name('__init__')
+    @gorilla.settings(allow_hit=True)
+    def patched__init__(self, func=None, inverse_func=None, *, validate=False, accept_sparse=False, check_inverse=True,
+                        kw_args=None, inv_kw_args=None, mlinspect_caller_filename=None, mlinspect_lineno=None,
+                        mlinspect_optional_code_reference=None, mlinspect_optional_source_code=None,
+                        mlinspect_fit_transform_active=False):
+        """ Patch for ('sklearn.preprocessing_function_transformer', 'FunctionTransformer') """
+        # pylint: disable=no-method-argument, attribute-defined-outside-init
+        original = gorilla.get_original_attribute(preprocessing.FunctionTransformer, '__init__')
+
+        self.mlinspect_caller_filename = mlinspect_caller_filename
+        self.mlinspect_lineno = mlinspect_lineno
+        self.mlinspect_optional_code_reference = mlinspect_optional_code_reference
+        self.mlinspect_optional_source_code = mlinspect_optional_source_code
+        self.mlinspect_fit_transform_active = mlinspect_fit_transform_active
+
+        def execute_inspections(_, caller_filename, lineno, optional_code_reference, optional_source_code):
+            """ Execute inspections, add DAG node """
+            original(self, func=func, inverse_func=inverse_func, validate=validate, accept_sparse=accept_sparse,
+                     check_inverse=check_inverse, kw_args=kw_args, inv_kw_args=inv_kw_args)
+
+            self.mlinspect_caller_filename = caller_filename
+            self.mlinspect_lineno = lineno
+            self.mlinspect_optional_code_reference = optional_code_reference
+            self.mlinspect_optional_source_code = optional_source_code
+
+        return execute_patched_func_no_op_id(original, execute_inspections, self, func=func, inverse_func=inverse_func,
+                                             validate=validate, accept_sparse=accept_sparse,
+                                             check_inverse=check_inverse, kw_args=kw_args, inv_kw_args=inv_kw_args)
+
+    @gorilla.name('fit_transform')
+    @gorilla.settings(allow_hit=True)
+    def patched_fit_transform(self, *args, **kwargs):
+        """ Patch for ('sklearn.preprocessing_function_transformer.FunctionTransformer', 'fit_transform') """
+        # pylint: disable=no-method-argument
+        self.mlinspect_fit_transform_active = True  # pylint: disable=attribute-defined-outside-init
+        original = gorilla.get_original_attribute(preprocessing.FunctionTransformer, 'fit_transform')
+        function_info = FunctionInfo('sklearn.preprocessing_function_transformer', 'FunctionTransformer')
+        input_info = get_input_info(args[0], self.mlinspect_caller_filename, self.mlinspect_lineno, function_info,
+                                    self.mlinspect_optional_code_reference, self.mlinspect_optional_source_code)
+
+        operator_context = OperatorContext(OperatorType.TRANSFORMER, function_info)
+        input_infos = SklearnBackend.before_call(operator_context, [input_info.annotated_dfobject])
+        result = original(self, input_infos[0].result_data, *args[1:], **kwargs)
+        backend_result = SklearnBackend.after_call(operator_context,
+                                                   input_infos,
+                                                   result)
+        new_return_value = backend_result.annotated_dfobject.result_data
+        if isinstance(input_infos[0].result_data, pandas.DataFrame):
+            columns = list(input_infos[0].result_data.columns)
+        else:
+            columns = ['array']
+
+        dag_node = DagNode(singleton.get_next_op_id(),
+                           BasicCodeLocation(self.mlinspect_caller_filename, self.mlinspect_lineno),
+                           operator_context,
+                           DagNodeDetails("Function Transformer: fit_transform", columns),
+                           get_optional_code_info_or_none(self.mlinspect_optional_code_reference,
+                                                          self.mlinspect_optional_source_code))
+        add_dag_node(dag_node, [input_info.dag_node], backend_result)
+        self.mlinspect_fit_transform_active = False  # pylint: disable=attribute-defined-outside-init
+        return new_return_value
+
+    @gorilla.name('transform')
+    @gorilla.settings(allow_hit=True)
+    def patched_transform(self, *args, **kwargs):
+        """ Patch for ('sklearn.preprocessing_function_transformer.FunctionTransformer', 'transform') """
+        # pylint: disable=no-method-argument
+        original = gorilla.get_original_attribute(preprocessing.FunctionTransformer, 'transform')
+        if not self.mlinspect_fit_transform_active:
+            function_info = FunctionInfo('sklearn.preprocessing_function_transformer', 'FunctionTransformer')
+            input_info = get_input_info(args[0], self.mlinspect_caller_filename, self.mlinspect_lineno, function_info,
+                                        self.mlinspect_optional_code_reference, self.mlinspect_optional_source_code)
+
+            operator_context = OperatorContext(OperatorType.TRANSFORMER, function_info)
+            input_infos = SklearnBackend.before_call(operator_context, [input_info.annotated_dfobject])
+            result = original(self, input_infos[0].result_data, *args[1:], **kwargs)
+            backend_result = SklearnBackend.after_call(operator_context,
+                                                       input_infos,
+                                                       result)
+            new_return_value = backend_result.annotated_dfobject.result_data
+            if isinstance(input_infos[0].result_data, pandas.DataFrame):
+                columns = list(input_infos[0].result_data.columns)
+            else:
+                columns = ['array']
+
+            dag_node = DagNode(singleton.get_next_op_id(),
+                               BasicCodeLocation(self.mlinspect_caller_filename, self.mlinspect_lineno),
+                               operator_context,
+                               DagNodeDetails("Function Transformer: transform", columns),
+                               get_optional_code_info_or_none(self.mlinspect_optional_code_reference,
+                                                              self.mlinspect_optional_source_code))
+            add_dag_node(dag_node, [input_info.dag_node], backend_result)
+        else:
+            new_return_value = original(self, *args, **kwargs)
+        return new_return_value
+
+
 @gorilla.patches(tree.DecisionTreeClassifier)
 class SklearnDecisionTreePatching:
     """ Patches for sklearn DecisionTree"""
