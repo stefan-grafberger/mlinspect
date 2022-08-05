8a809e57b640556e341f860c1c75ec0515629d4b
Stefan Grafberger <18442112+stefan-grafberger@users.noreply.github.com>
2022-08-05 14:03:04 +0200

    test_numpy_ravel

--- test/monkeypatching/test_patch_numpy.py
+++ test/monkeypatching/test_patch_numpy.py
@@ -43,3 +43,35 @@ def test_numpy_random():
                                     columns=['array', 'mlinspect_lineage_0_0'])
     pandas.testing.assert_frame_equal(lineage_output.reset_index(drop=True), expected_lineage_df.reset_index(drop=True),
                                       atol=1)
+
+
+def test_numpy_ravel():
+    """
+    Tests whether the monkey patching of ('numpy.random', 'random') works
+    """
+    test_code = cleandoc("""
+        import numpy as np
+        np.random.seed(42)
+        test = np.random.random(100)
+        test = test.ravel()
+        assert test._mlinspect_annotation is not None
+        """)
+
+    inspector_result = _pipeline_executor.singleton.run(python_code=test_code, track_code_references=True,
+                                                        inspections=[RowLineage(2)])
+    extracted_node: DagNode = list(inspector_result.dag.nodes)[0]
+
+    expected_node = DagNode(0,
+                            BasicCodeLocation("<string-source>", 3),
+                            OperatorContext(OperatorType.DATA_SOURCE, FunctionInfo('numpy.random', 'random')),
+                            DagNodeDetails('random', ['array']),
+                            OptionalCodeInfo(CodeReference(3, 7, 3, 28), "np.random.random(100)"))
+    compare(extracted_node, expected_node)
+
+    inspection_results_data_source = inspector_result.dag_node_to_inspection_results[extracted_node]
+    lineage_output = inspection_results_data_source[RowLineage(2)]
+    expected_lineage_df = DataFrame([[0.5, 0],
+                                     [0.5, 1]],
+                                    columns=['array', 'mlinspect_lineage_0_0'])
+    pandas.testing.assert_frame_equal(lineage_output.reset_index(drop=True), expected_lineage_df.reset_index(drop=True),
+                                      atol=1)
