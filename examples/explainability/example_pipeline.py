f6adb212b11b876fc346f0e5e683d2f6b65e56f3
PiStefania <stefania.patsou@hotmail.com>
2023-05-29 22:03:28 +0300

    create notebook and tweak keras and tensorflow version

---
+++ examples/explainability/example_pipeline.py
@@ -0,0 +1,58 @@
+"""Predicting which patients are at a higher risk of complications"""
+import warnings
+import os
+import pandas as pd
+import shap as shap
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from example_pipelines.healthcare.healthcare_utils import MyW2VTransformer, MyKerasClassifier, \
+    create_model
+from mlinspect.utils import get_project_root
+
+def f(model, X):
+    return model.predict([X[:,i] for i in range(X.shape[1])]).flatten()
+
+# FutureWarning: Sklearn 0.24 made a change that breaks remainder='drop', that change will be fixed
+#  in an upcoming version: https://github.com/scikit-learn/scikit-learn/pull/19263
+warnings.filterwarnings('ignore')
+
+COUNTIES_OF_INTEREST = ['county2', 'county3']
+
+patients = pd.read_csv(os.path.join(str(get_project_root()), "example_pipelines", "healthcare",
+                                    "patients.csv"), na_values='?')
+histories = pd.read_csv(os.path.join(str(get_project_root()), "example_pipelines", "healthcare",
+                                     "histories.csv"), na_values='?')
+
+data = patients.merge(histories, on=['ssn'])
+complications = data.groupby('age_group') \
+    .agg(mean_complications=('complications', 'mean'))
+data = data.merge(complications, on=['age_group'])
+data['label'] = data['complications'] > 1.2 * data['mean_complications']
+data = data[['smoker', 'last_name', 'county', 'num_children', 'race', 'income', 'label']]
+data = data[data['county'].isin(COUNTIES_OF_INTEREST)]
+
+impute_and_one_hot_encode = Pipeline([
+    ('impute', SimpleImputer(strategy='most_frequent')),
+    ('encode', OneHotEncoder(sparse=False, handle_unknown='ignore'))
+])
+featurisation = ColumnTransformer(transformers=[
+    ("impute_and_one_hot_encode", impute_and_one_hot_encode, ['smoker', 'county', 'race']),
+    ('word2vec', MyW2VTransformer(min_count=2), ['last_name']),
+    ('numeric', StandardScaler(), ['num_children', 'income']),
+], remainder='drop')
+neural_net = MyKerasClassifier(build_fn=create_model, epochs=10, batch_size=1, verbose=0)
+pipeline = Pipeline([
+    ('features', featurisation),
+    ('learner', neural_net)])
+
+train_data, test_data = train_test_split(data)
+model = pipeline.fit(train_data, train_data['label'])
+print("Mean accuracy: {}".format(model.score(test_data, test_data['label'])))
+print("Shape of data: {}, train: {} and test: {}".format(data.shape, train_data.shape, test_data.shape))
+# Introduce explainability
+explainer = shap.KernelExplainer(f, model, data.iloc[:50,:])
+shap_values = explainer.shap_values(test_data.iloc[299,:], nsamples=500)
+#shap.force_plot(explainer.expected_value, shap_values, X_display.iloc[299,:])
