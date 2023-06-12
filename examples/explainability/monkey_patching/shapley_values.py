580165c9325aca20f1a49eebba1b195434bcaa30
PiStefania <stefania.patsou@hotmail.com>
2023-06-12 15:59:21 +0300

    create tmp file

---
+++ examples/explainability/monkey_patching/shapley_values.py
@@ -0,0 +1,59 @@
+"""
+The ShapleyValues Inspection
+"""
+import dataclasses
+from _pydecimal import Decimal
+from typing import Iterable, Dict
+
+from mlinspect import FunctionInfo
+from mlinspect.inspections import Inspection, InspectionInputUnaryOperator
+
+
+@dataclasses.dataclass(frozen=True, eq=True)
+class ShapleyValuesInfo:
+    values: Dict[str, Decimal]
+
+
+class ShapleyValues(Inspection):
+
+    def __init__(self):
+        self._is_embedding_operator = False
+        self._missing_embedding_count = 0
+        self._missing_embeddings_examples = []
+
+    def visit_operator(self, inspection_input) -> Iterable[any]:
+        """
+        Visit an operator
+        """
+        # pylint: disable=too-many-branches, too-many-statements
+        if isinstance(inspection_input, InspectionInputUnaryOperator) and \
+                inspection_input.operator_context.function_info == \
+                FunctionInfo('example_pipelines.healthcare.healthcare_utils', 'MyW2VTransformer'):
+            # TODO: Are there existing word embedding transformers for sklearn we can use this for?
+            self._is_embedding_operator = True
+            for row in inspection_input.row_iterator:
+                # Count missing embeddings
+                embedding_array = row.output[0]
+                is_zero_vector = not embedding_array.any()
+                if is_zero_vector:
+                    self._missing_embedding_count += 1
+                    if len(self._missing_embeddings_examples) < self.example_threshold:
+                        self._missing_embeddings_examples.append(row.input[0])
+            yield None
+        else:
+            for _ in inspection_input.row_iterator:
+                yield None
+
+    def get_operator_annotation_after_visit(self) -> any:
+        if self._is_embedding_operator:
+            assert self._missing_embedding_count is not None  # May only be called after the operator visit is finished
+            result = MissingEmbeddingsInfo(self._missing_embedding_count, self._missing_embeddings_examples)
+            self._missing_embedding_count = 0
+            self._is_embedding_operator = False
+            self._missing_embeddings_examples = []
+            return result
+        return None
+
+    @property
+    def inspection_id(self):
+        return self.example_threshold
